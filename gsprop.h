89c891819ceeb4874ee478e3b775891b1e6f1be6
Furetto126 <95306470+Furetto126@users.noreply.github.com>
2025-03-06 19:33:49 +0100

    Initial commit

---
+++ gsprop.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <functional>
+
+template<typename T>
+class GSProperty {
+private:
+        T value;
+        
+        std::function<T(const T&)> setter;
+        std::function<T(const T&)> getter;
+public:
+        explicit GSProperty(
+                T value,
+                std::function<T(const T&)> s = [](const T& val) { return val; },
+                std::function<T(const T&)> g = [](const T& val) { return val; }
+        ) : value(value), getter(std::move(g)), setter(std::move(s)) {}
+        
+        operator T() const {
+                return getter(value);
+        }
+        
+        T& operator=(const T& newValue) {
+                value = setter(newValue);
+                return value;
+        }
+        
+        GSProperty& operator=(const GSProperty& other) {
+                if (this != &other) value = setter(static_cast<T>(other));
+                return *this;
+        }
+};
+
+
